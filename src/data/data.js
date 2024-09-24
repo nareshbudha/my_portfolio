a652de1395296a5279083aa722180151589ea657
nareshbudha <nareshkumarbudha@gmail.com>
2024-09-24 22:02:37 +0545

    adding more work

--- src/data/data.js
+++ src/data/data.js
@@ -1,18 +1,19 @@
 import bnp from '../assets/birendnagar.png'
 import taj from '../assets/tajgroup.png'
+import espell from '../assets/spell-e.png'
 
 export const data = [
   {
     id: 1,
-    img: 'https://i.postimg.cc/Bv75CLyN/4167022.jpg',
-    title: "Tarvel Agency",
+    img: espell,
+    title: "spell Ecommerce",
     link: "",
     git: "",
   },
   {
     id: 2,
     img: taj,
-    title: "",
+    title: " Taj Group Nepal ",
     desc: "The Taj Group Nepal website, built with React.js and Tailwind CSS, offers a clean, responsive design featuring a horizontally scrolling product slider with smooth animations that allow users to interactively browse products. The site includes filter functionality, enabling users to sort products by specific criteria such as category or popularity, making navigation intuitive and efficient. Subtle animations enhance the overall user experience, creating an engaging and dynamic interface without overwhelming the content. The website effectively integrates modern UI/UX principles, delivering a seamless and user-friendly browsing experience.",
     link: "https://tajgroupnepal.com/",
     git: "",
