8e61a426c8dd07520503c04e3b30b7eba3dfe08c
nareshbudha <nareshkumarbudha@gmail.com>
2026-05-02 20:05:38 +0545

    upated the auto date in the copyright in the footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -8,12 +8,12 @@ const Footer = () => {
     <div id="works" className=" mx-auto m-auto h-[300px]  mt-16 sm:h-[250px]">
 
       <div className=" bg-red-900 h-full flex flex-col gap-8 items-center justify-between p-10 sm:p-7">
-        <h2 
-        data-aos="zoom-out"
-         className=" font-bold text-5xl sm:text-3xl">Let's Talk</h2>
+        <h2
+          data-aos="zoom-out"
+          className=" font-bold text-5xl sm:text-3xl">Let's Talk</h2>
         <div className=" flex items-center justify-center gap-8 sm:gap-5">
-          <a 
-          data-aos="fade-up" data-aos-duration="1000"
+          <a
+            data-aos="fade-up" data-aos-duration="1000"
 
             href=""
             className="box font-medium text-white   flex items-center justify-center flex-col"
@@ -21,8 +21,8 @@ const Footer = () => {
             <FaTelegramPlane className=" text-black text-3xl hover:scale-125 cursor-pointer" />
             <p>Telegram</p>
           </a>
-          <a 
-          data-aos="fade-up" data-aos-duration="1200"
+          <a
+            data-aos="fade-up" data-aos-duration="1200"
 
             href=""
             className="box font-medium text-white  flex items-center justify-center flex-col"
@@ -30,8 +30,8 @@ const Footer = () => {
             <IoLogoWhatsapp className=" text-black text-3xl hover:scale-125 cursor-pointer" />
             <p>WhatsApp</p>
           </a>
-          <a 
-          data-aos="fade-up" data-aos-duration="1400"
+          <a
+            data-aos="fade-up" data-aos-duration="1400"
 
             href=""
             className="box font-medium text-white  flex items-center justify-center flex-col"
@@ -41,8 +41,8 @@ const Footer = () => {
           </a>
         </div>
         <div className="sm:text-[12px]">
-          | Copyright &copy; <span>2024 Naresh Kumar Budha</span> All rights reserved
-          <a href="#"></a> |
+          | Copyright &copy; {new Date().getFullYear()}{" "}
+          <span>Naresh Kumar Budha</span> All rights reserved |
         </div>
       </div>
 
